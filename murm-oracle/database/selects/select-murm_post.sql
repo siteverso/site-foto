c2903fda4b4798ca0902c0529fb37d33066b754a
Daniel <danielmaiax@gmail.com>
2026-07-12 03:47:03 -0300

    ...

--- murm-oracle/database/selects/select-murm_post.sql
+++ murm-oracle/database/selects/select-murm_post.sql
@@ -6,3 +6,11 @@ order by id desc
 select distinct post_type
 from murm_post
 ;
+
+
+UPDATE murm_user
+   SET password_hash = '$2b$12$lyWWL6rcX9267HwZvM.GCuUGp3Gib5RNAEkp189AN7dd0USObqyay',
+       updated_at = SYSTIMESTAMP
+ WHERE LOWER(username) = 'machine';
+
+COMMIT;
